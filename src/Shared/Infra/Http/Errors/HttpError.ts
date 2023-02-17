eaddb69bd677bf10a4a50e2619c656021932cf79
gordao-dev <contato.gmds@gmail.com>
2023-02-17 14:07:50 -0300

    feat(LinkSHortener): Http error

---
+++ src/Shared/Infra/Http/Errors/HttpError.ts
@@ -0,0 +1,14 @@
+export class HttpError extends Error {
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    super(message);
+
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default HttpError;
